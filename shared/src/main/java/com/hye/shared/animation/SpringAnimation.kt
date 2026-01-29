275ad7cd9c3ad305e471b45cbfcfc74b58b29c00
deoya <deoya@naver.com>
2026-01-29 23:39:25 +0900

    WIP: 공통 애니메이션 적용 실험 중 (완성 안됨)

---
+++ shared/src/main/java/com/hye/shared/animation/SpringAnimation.kt
@@ -0,0 +1,33 @@
+package com.hye.shared.animation
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import kotlinx.coroutines.launch
+
+@Composable
+fun lowSpringAnimation(durationMillis:Int = 800 , dampingRatio: Float = 0.6f): Pair<Float, Float> {
+    val scale = remember { Animatable(0f) }
+    val alpha = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        launch {
+            alpha.animateTo(
+                1f,
+                tween(durationMillis)
+            )
+        }
+        launch {
+            scale.animateTo(
+                1f,
+                spring(dampingRatio = 0.6f, stiffness = Spring.StiffnessLow)
+            )
+        }
+    }
+
+    return Pair(scale.value, alpha.value)
+}
