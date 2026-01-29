3870d14873c2c12a8cf12bac2c76ead432d34c99
deoya <deoya@naver.com>
2026-01-29 21:58:01 +0900

    [BUILD] Coil 라이브러리 의존성추가

--- app/src/main/java/com/hye/healthpossible/HealthpossibleApplication.kt
+++ app/src/main/java/com/hye/healthpossible/HealthpossibleApplication.kt
@@ -1,15 +1,28 @@
 package com.hye.healthpossible
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class HealthpossibleApplication : Application(){
+class HealthpossibleApplication : Application(), SingletonImageLoader.Factory{
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
     override fun onCreate() {
         super.onCreate()
         healthpossibleApplication = this
         ToastHelper.init(this)
     }
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return imageLoader
+    }
+
     companion object{
         private lateinit var healthpossibleApplication: HealthpossibleApplication
         fun getAppContext() = healthpossibleApplication
